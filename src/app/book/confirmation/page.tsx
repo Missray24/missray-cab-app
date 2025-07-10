030d545048107af4940e60e0eef516524bef2556
Abderaouf Nasri <nasri.ar06@gmail.com>
2025-07-10 08:41:37 +0000

    afficher ce texte Paiement:  a la place de l'icon (for element <DollarSi

--- src/app/book/confirmation/page.tsx
+++ src/app/book/confirmation/page.tsx
@@ -157,7 +157,7 @@ function ConfirmationComponent() {
                       </div>
                     )}
                     <div className="flex items-center gap-3">
-                        <span className="text-sm w-12 text-muted-foreground">Paiement</span>
+                        <span className="text-sm w-auto pr-2 text-muted-foreground">Paiement:</span>
                         <p>{reservation.amount.toFixed(2)}€ ({reservation.paymentMethod})</p>
                     </div>
                 </div>
